f7345e53ad336acb2307a354d8708a2efb8be10d
hnnav <henna.v@icloud.com>
2020-04-07 14:01:49 +0300

    changes

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -4,4 +4,27 @@ require "./app/models/trip"
 
 class TripController < ApplicationController
 
+  # CREATE
+    # render form
+  get '/trips/new' do
+    erb ':trips/new'
+  end
+  
+  # create instaces
+
+  # READ
+  # show one trip
+
+
+  # show all trips
+  # connection: trips/index.erb
+
+
+  # UPDATE
+  # render update form (trips/eit.erb)
+  # update instance
+
+
+  # DELETE
+  # a button to show page?
 end
