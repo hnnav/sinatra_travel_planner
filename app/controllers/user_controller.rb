f55aa989afd885b743f22f82c310b5c78943cb62
hnnav <henna.v@icloud.com>
2020-04-08 12:56:28 +0300

    added login page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,20 +4,24 @@ require "./app/models/trip"
 
 class UserController < ApplicationController
 
-    # Render signup form
-    get '/users/signup' do
-        erb ':users/signup'
+  get '/users/login' do
+    erb :'users/login'
+  end
+
+  # Render signup form
+  get '/users/signup' do
+      erb :'users/signup'
+  end
+  
+  # create user instances
+  post '/users/signup' do    
+      @user = User.create(username: params[:username], password: params[:password])
+      redirect "/users/#{@user.id}"
     end
     
-    # create user instances
-    post '/users/signup' do    
-        @user = User.create(username: params[:username], password: params[:password])
-        redirect "/users/#{@user.id}"
-      end
-      
-      # dynamic route for show page
-      get '/users/:id' do
-        @user = User.find(params[:id]) 
-        erb :'users/show'
-      end
+  # dynamic route for show page
+  get '/users/:id' do
+    @user = User.find(params[:id]) 
+    erb :'users/show'
+  end
 end
